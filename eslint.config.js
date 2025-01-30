b45b55c570ee0e31bf809daa8ce55d8e14acce3f
Ahmad Ihsan <ihsanashi@gmail.com>
2025-01-30 12:34:45 +0800

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,11 @@
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import pluginJs from '@eslint/js';
+
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default [
-  {
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-    },
-    rules: {
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-    },
-  },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
