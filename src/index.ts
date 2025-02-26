68c9816cdd92381f4b9fa2ed9db92884a653fe52
Ahmad Ihsan <ihsanashi@gmail.com>
2025-02-26 10:20:39 +0800

    chore: disable finance tracker cronjob

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config';
 import express, { Express, Request, Response } from 'express';
 
-import { FinanceTrackerCronjob } from '@cron/finance-tracker';
-
 import chatRoutes from '@routes/chat';
 import diaryRoutes from '@routes/diary';
 
@@ -22,6 +20,4 @@ app.use('/diary', diaryRoutes);
 
 app.listen(port, () => {
   logger.info(`[server]: Server is running on port ${port}`);
-
-  FinanceTrackerCronjob.main();
 });
